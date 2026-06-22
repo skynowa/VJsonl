e9a89622f5bbd009f8ffacb34c954f1c82543b51
skynowa <skynowa@gmail.com>
2026-06-22 12:48:25 +0300

    JsonlModel.h [1+] - Request column - add

--- src/JsonlModel.h
+++ src/JsonlModel.h
@@ -78,6 +78,7 @@ private:
         // QStringLiteral("error"),
         QStringLiteral("msg"),
         QStringLiteral("query"),
+        QStringLiteral("request"),
         QStringLiteral("descr"),
         QStringLiteral("request_time"),
         // QStringLiteral("message"),
